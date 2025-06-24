277e6b847b0b4cdb401d376cba5b8425d7512494
TempUser <temp@mail>
2025-06-24 22:03:52 +0300

    Color tags support for logger

--- src/main/Log.java
+++ src/main/Log.java
@@ -1,26 +1,65 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Log {
+
+	private static HashMap<String, Colors> colors = new HashMap<>();
 	
-	public static enum Errors {
+	static {
+		for (var c : Colors.values()) {
+			colors.put(c.name, c);
+			System.out.println("added: " + c.toString()  + c.name + "\033[0m");
+		}
+	}
+	
+	public static enum Colors {
+
+		red(31),
+		green(32),
+		yellow(33),
+		magenta(35),
+		lightGreen(92),
+		lightYellow(93),
+		reset(0);
+
+		String color, name = name();
 		
+		private Colors(int code) {
+			color = code == 0 ? "\033[0m" : "\033[1;" + code + "m";
+			StringBuilder name = new StringBuilder();
+			for (int i = 0; i < this.name.length(); i++) {
+				char c = this.name.charAt(i);
+				if(Character.isUpperCase(c)) {
+					name.append('-');
+					c = Character.toLowerCase(c);
+				}
+				name.append(c);
+			}
+			this.name = name.toString();
+		}
 		
+		@Override
+		public String toString() {
+			return color;
+		}
 	}
 
 	public static void info(Object... args) {
-		System.out.println(getCaller() + format("", args));
+		System.out.println(getCaller() + paint(format("", args)));
 	}
 
 	public static void info(String text, Object... args) {
-		System.out.println(getCaller() + format(text, args));
+		System.out.println(getCaller() + paint(format(text, args)));
 	}
 
 	public static void warn(Object... args) {
-		System.out.println(getCaller() + format("", args));
+		System.out.println(getCaller() + paint(format("[yellow]", args) + "[]"));
 	}
 	
 	public static void warn(String text, Object... args) {
-		System.out.println(getCaller() + format(text, args));
+		System.out.println(getCaller() + paint("[yellow]" + format(text, args) + "[]"));
 	}
 	
 	private static String getCaller() {
@@ -50,10 +89,6 @@ public class Log {
 		System.err.println(getCaller() + format(text, args));
 	}
 	
-	public static void err(Errors error) {
-		System.err.println(error);
-	}
-	
 	private static String format(String str, Object... args) {
 		StringBuilder formated = new StringBuilder(str.length());
 		int arg = 0;
@@ -69,15 +104,59 @@ public class Log {
 			if(formated.length() != 0) formated.append(' ');
 			formated.append(toString(args[i]));
 		}
-		formated.append("\033[0m ");
 		return formated.toString();
 	}
 	
+	private static String paint(String str) {
+		Stack<Colors> stack = new Stack<Log.Colors>();
+		stack.push(Colors.reset);
+		StringBuilder colored = new StringBuilder();
+		
+		int begin = -1;
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			
+			if(c == '[') {
+				begin = i+1;
+				continue;
+			}
+			if(c == ']' && begin != -1) {
+				String tag = str.substring(begin, i);
+				begin = -1;
+				if(tag.length() == 0) {
+					if(stack.size() > 0) {
+						stack.pop();
+						colored.append(stack.peek());
+					} else {
+						colored.append("[]");
+					}
+					continue;
+				}
+				Colors color =colors.get(tag);
+				if(color == null) {
+					colored.append('[');
+					colored.append(tag);
+					colored.append(']');
+				} else {
+					colored.append(color);
+					stack.push(color);
+				}
+				continue;
+			}
+			if(begin != -1) continue;
+			colored.append(c);
+		}
+		if(begin != -1) {
+			colored.append(str.substring(begin-1, str.length()));
+		}
+		return colored.toString();
+	}
+	
 	private static String toString(Object object) {
-		if(object == null) return "\033[1;35mnull\033[0m";
+		if(object == null) return "[magenta]null[]";
 		if(object instanceof Boolean) {
 			Boolean b = (Boolean) object;
-			return b ? "\033[1;32mtrue \033[0m" : "\033[1;31mfalse\033[0m";
+			return b ? "[green]true[]" : "[red]false[]";
 		}
 		
 		return object.toString();
